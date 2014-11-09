73985081f71aa93962e0ce8ab811bb78054527bd
zfc5840 <1076231986@qq.com>
2014-11-09 12:41:13 +0000

    创建项目castor，用于xml与object之间进行转换

---
+++ src/main/java/com/demo/java_castor_xml_object/App.java
@@ -0,0 +1,61 @@
+package com.demo.java_castor_xml_object;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 public static void main( String[] args )
+	    {
+	        
+	    	try {
+				
+		    	//( 1 ) OBJECT --> XML
+	    		FileWriter writer = new FileWriter("article.xml");
+				Marshaller.marshal(createArticle(), writer);
+				writer.close();
+				
+				
+		    	//( 2 ) XML --> OBJECT
+				FileReader reader = new FileReader("article.xml");
+				Article article = (Article) Unmarshaller.unmarshal(Article.class, reader);
+				
+				System.out.println(article);
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (MarshalException e) {
+				e.printStackTrace();
+			} catch (ValidationException e) {
+				e.printStackTrace();
+			}
+
+	    }
+	    
+	    public static Article createArticle(){
+	    	Article article = new Article();
+	    	
+	    	article.setTitle("Castor - Java Object to XML & XML to Object Mapping");
+	    	article.setUrl("http://hmkcode.com/castor-java-object-xml");
+	    	article.addCategory("Java");
+	    	article.addTag("Java");
+	    	article.addTag("Castor");
+	    	article.addTag("XML");
+	    	article.addTag("Marshalling");
+	    	article.addTag("Unmarshalling");
+	    	
+	    	return article;
+	    }
+}
